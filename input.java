95683568cb4cb6e0afba22e66dc3ac38668b4ab8
Yogesh Nagre <yogeshnagre000@gmail.com>
2025-07-01 11:40:09 +0530

    add java file

---
+++ input.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+class helloword {
+    
+    public static void main(String[] args) {
+        double x, y, sum;
+    Scanner myNum = new Scanner(System.in); 
+    //System.out.println("Type a number:");
+    x = myNum.nextDouble(); 
+
+    //System.out.println("Type another number:");
+    y = myNum.nextDouble(); 
+
+    sum = x + y; 
+    System.out.println("Sum is: " + sum); 
+    myNum.close(); 
+  }
+} 
